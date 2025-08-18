eda502b8ea16f74828958ab2ab010d0873add0f6
Oleksandra Kostenko <sashakost96@gmail.com>
2025-08-18 17:42:54 +0300

    Updete realisation of an extra task2 for Lesson 6

--- extra3.2_HW_6.1.js
+++ extra3.2_HW_6.1.js
@@ -5,7 +5,7 @@ Numbers may be positive or negative, integers or floats.
 Floats with decimal part non equal to zero are considered UNeven for this kata. */
 
 const isEven = (number) =>
-  typeof number != "number" ? false : number % 2 === 0;
+!Number.isInteger(number) ? false : number % 2 === 0;
 
 console.log(`Even(28) -> ` + isEven(28));
 console.log(`Not Even(5) -> ` + isEven(5));
@@ -13,3 +13,4 @@ console.log(`Neg Even(-6) ->` + isEven(6));
 console.log(`Neg not Even(-7) -> ` + isEven(-7));
 console.log(`Float (56.4) -> ` + isEven(56.4));
 console.log(`Neg Float(-6.4) -> ` + isEven(-6.4));
+console.log(`Not Even(char) -> ` + isEven("28"));
