3ef2aa860b9309ca69d3210f7eaee51d3fcc72fe
Oleksandra Kostenko <sashakost96@gmail.com>
2025-08-18 20:17:40 +0300

    Added realisation of an extra task3 for Lesson 6

---
+++ extra3.3_HW_6.1.js
@@ -0,0 +1,24 @@
+/* Task:
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the
+mess behind you.
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more
+days, you get $20 off your total.
+Write a code that gives out the total amount for different days(d). */
+
+function countTotal(days) {
+  const dayRent = 40;
+  const sevenDaysDiscount = 50;
+  const threeDaysDiscount = 20;
+  let totalPrice = days * dayRent;
+  
+  if (days >= 7) {
+    totalPrice -=  sevenDaysDiscount;
+  } else if (days >= 3) {
+    totalPrice -= threeDaysDiscount;
+  }
+  return totalPrice;
+}
+
+console.log(`Total Price is: ` + countTotal(10) + "$");
